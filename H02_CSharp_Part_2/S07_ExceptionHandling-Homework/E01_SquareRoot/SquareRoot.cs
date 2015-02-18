51a0dbfb8e72c3b564e074e935cd90e90eff199b
Boray <boray@abv.bg>
2015-02-18 22:42:25 +0200

    ExceptionHandling-Homework - Done

---
+++ H02_CSharp_Part_2/S07_ExceptionHandling-Homework/E01_SquareRoot/SquareRoot.cs
@@ -0,0 +1,41 @@
+﻿namespace E01_SquareRoot
+{
+    using System;
+
+    public class SquareRoot
+    {
+        public static void Main(string[] args)
+        {
+            // Write a program that reads an integer number and 
+            // calculates and prints its square root.
+            // If the number is invalid or negative, print Invalid number.
+            // In all cases finally print Good bye.
+            // Use try-catch-finally block.
+
+            try
+            {
+                Console.Write("Please, enter an integer: ");
+                int number = int.Parse(Console.ReadLine().Trim());
+
+                if (number < 0)
+                {
+                    throw new ArgumentException("Invalid number");
+                }
+
+                Console.WriteLine("Sqrt({0}) = {1}", number, Math.Sqrt(number));
+            }
+            catch (ArgumentException ae)
+            {
+                Console.WriteLine(ae.Message);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Invalid number !");
+            }
+            finally
+            {
+                Console.WriteLine("Good bye !");
+            }
+        }
+    }
+}
---
+++ H02_CSharp_Part_2/S07_ExceptionHandling-Homework/E03_ReadFileContents/ReadFileContents.cs
@@ -0,0 +1,61 @@
+﻿namespace E03_ReadFileContents
+{
+    using System;
+    using System.IO;
+    using System.Security;
+
+    public class ReadFileContents
+    {
+        public static void Main(string[] args)
+        {
+            // Write a program that enters file name along with its 
+            // full file path (e.g. C:\WINDOWS\win.ini), reads its 
+            // contents and prints it on the console.
+            //Find in MSDN how to use System.IO.File.ReadAllText(…).
+            //Be sure to catch all possible exceptions and print 
+            // user-friendly error messages.
+
+            Console.Write("Enter the full name and path of file:");
+            try
+            {
+                Console.WriteLine(System.IO.File.ReadAllText(Console.ReadLine()));
+            }
+            catch (DirectoryNotFoundException dnfe)
+            {
+                Console.WriteLine(dnfe.Message);
+            }
+            catch (FileNotFoundException fnfe)
+            {
+                Console.WriteLine(fnfe.Message);
+            }
+            catch (ArgumentNullException ane)
+            {
+                Console.WriteLine(ane.Message);
+            }
+            catch (ArgumentException ae)
+            {
+                Console.WriteLine(ae.Message);
+            }
+            catch (PathTooLongException ptle)
+            {
+                Console.WriteLine(ptle.Message);
+            }
+            catch (UnauthorizedAccessException uae)
+            {
+                Console.WriteLine(uae.Message);
+            }
+            catch (SecurityException se)
+            {
+                Console.WriteLine(se.Message);
+            }
+            catch (NotSupportedException nse)
+            {
+                Console.WriteLine(nse.Message);
+            }
+            catch (IOException ioe)
+            {
+                Console.WriteLine(ioe.Message);
+            }
+        }
+    }
+}
