e97f4aaf66eb64879c4b0a8fd985f06561ac7a7c
Boray <boray@abv.bg>
2015-06-23 22:20:50 +0300

    S01_CodeFormatting - Done !

---
+++ H08_High_Quality_Code/S01_CodeFormatting/E01_CSharp/EventHolder.cs
@@ -0,0 +1,62 @@
+﻿namespace E01_CSharp
+{
+    using System;
+    using Wintellect.PowerCollections;
+
+    public class EventHolder
+    {
+        private MultiDictionary<string, Event> holdByTitle = new MultiDictionary<string, Event>(true);
+
+        private OrderedBag<Event> holdByDate = new OrderedBag<Event>();
+
+        public void AddEvent(DateTime date, string title, string location)
+        {
+            Event newEvent = new Event(date, title, location);
+
+            this.holdByTitle.Add(title.ToLower(), newEvent);
+            this.holdByDate.Add(newEvent);
+
+            Messages.EventAdded();
+        }
+
+        public void DeleteEvents(string titleToDelete)
+        {
+            string title = titleToDelete.ToLower();
+            int removed = 0;
+
+            foreach (var eventToRemove in this.holdByTitle[title])
+            {
+                removed++;
+                this.holdByDate.Remove(eventToRemove);
+            }
+
+            this.holdByTitle.Remove(title);
+
+            Messages.EventDeleted(removed);
+        }
+
+        public void ListEvents(DateTime date, int count)
+        {
+            OrderedBag<Event>.View eventsToShow = this.holdByDate.RangeFrom(
+                new Event(date, string.Empty, string.Empty), true);
+
+            int showed = 0;
+
+            foreach (var eventToShow in eventsToShow)
+            {
+                if (showed == count)
+                {
+                    break;
+                }
+
+                Messages.PrintEvent(eventToShow);
+                showed++;
+            }
+
+            if (showed == 0)
+            {
+                Messages.NoEventsFound();
+            }
+        }
+    }
+}
