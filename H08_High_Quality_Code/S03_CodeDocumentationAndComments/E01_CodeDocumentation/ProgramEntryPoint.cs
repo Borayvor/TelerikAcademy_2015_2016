8ebad8b176819443c553d94b61cf623a19ea9be3
Boray <boray@abv.bg>
2015-07-04 11:35:04 +0300

    S03_CodeDocumentationAndComments - Done !

---
+++ H08_High_Quality_Code/S03_CodeDocumentationAndComments/E01_CodeDocumentation/ProgramEntryPoint.cs
@@ -0,0 +1,23 @@
+﻿namespace E01_CodeDocumentation
+{
+    using System;
+
+    /// <summary>
+    /// Main class of the program
+    /// </summary>
+    public class ProgramEntryPoint
+    {
+        /// <summary>
+        /// Main method
+        /// </summary>
+        public static void Main()
+        {
+            string str = "42";
+
+            int strAsInt = str.ToInteger();
+
+            Console.WriteLine("str + 2 = {0}", str + 2);
+            Console.WriteLine("strAsInt + 2 = {0}", strAsInt + 2);
+        }
+    }
+}
