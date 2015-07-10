420528e89b4935e7bb3793687e522529b9d44b55
Boray <boray@abv.bg>
2015-07-10 12:02:38 +0300

    S04_VariablesDataExpressionConst - Done !

---
+++ H08_High_Quality_Code/S04_VariablesDataExpressionConst/E01_ClassSizeInCSharp/Rectangle.cs
@@ -0,0 +1,28 @@
+﻿namespace E01_ClassSizeInCSharp
+{
+    public class Rectangle
+    {
+        public Rectangle(double width, double height)
+        {
+            this.Width = width;
+            this.Height = height;
+        }
+        
+        public double Width { get; private set; }
+
+        public double Height { get; private set; }
+
+        public Rectangle GetRotatedSize(Rectangle rotatedSize, double rotatedAngle)
+        {
+            double newWidth = (Math.Abs(Math.Cos(rotatedAngle)) * rotatedSize.Width) 
+                + (Math.Abs(Math.Sin(rotatedAngle)) * rotatedSize.Height);
+
+            double newHeight = (Math.Abs(Math.Sin(rotatedAngle)) * rotatedSize.Width)
+                + (Math.Abs(Math.Cos(rotatedAngle)) * rotatedSize.Height);
+
+            var rotatedRectangle = new Rectangle(newWidth, newHeight);
+
+            return rotatedRectangle;
+        }
+    }
+}
---
+++ H08_High_Quality_Code/S04_VariablesDataExpressionConst/E02_MethodPrintStatisticsInCSharp/Program.cs
@@ -0,0 +1,98 @@
+﻿namespace E02_MethodPrintStatisticsInCSharp
+{
+    using System;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Refactor the following code to apply variable usage and naming best practices:
+
+            //            public void PrintStatistics(double[] arr, int count)
+            //{
+            //    double max, tmp;
+            //    for (int i = 0; i < count; i++)
+            //    {
+            //        if (arr[i] > max)
+            //        {
+            //            max = arr[i];
+            //        }
+            //    }
+            //    PrintMax(max);
+            //    tmp = 0;
+            //    max = 0;
+            //    for (int i = 0; i < count; i++)
+            //    {
+            //        if (arr[i] < max)
+            //        {
+            //            max = arr[i];
+            //        }
+            //    }
+            //    PrintMin(max);
+
+            //    tmp = 0;
+            //    for (int i = 0; i < count; i++)
+            //    {
+            //        tmp += arr[i];
+            //    }
+            //    PrintAvg(tmp/count);
+            //}
+
+        }
+
+
+        public static void PrintStatistics(double[] array)
+        {
+            Console.WriteLine("The max value is: {0}", GetMaxValue(array));
+
+            Console.WriteLine("The average value is: {0}", GetAverageValue(array));
+
+            Console.WriteLine("The min value is: {0}", GetMinValue(array));
+        }
+
+        private static double GetMaxValue(double[] array)
+        {
+            double maxValue = double.MinValue;
+
+            for (int index = 0; index < array.Length; index++)
+            {
+                if (array[index] > maxValue)
+                {
+                    maxValue = array[index];
+                }
+            }
+
+            return maxValue;
+        }
+
+        private static double GetMinValue(double[] array)
+        {
+            double minValue = double.MaxValue;
+
+            for (int index = 0; index < array.Length; index++)
+            {
+                if (array[index] < minValue)
+                {
+                    minValue = array[index];
+                }
+            }
+
+            return minValue;
+        }
+
+        private static double GetAverageValue(double[] array)
+        {
+            double averageValue = 0;
+            double sum = 0;
+
+            for (int index = 0; index < array.Length; index++)
+            {
+                sum += array[index];
+            }
+
+            averageValue = sum / array.Length;
+
+            return averageValue;
+        }
+    }
+}
