cc8602dca10e920535107f1e597f3bbbf0960fe4
Boray <boray@abv.bg>
2015-07-20 10:23:39 +0300

    S06_HighQualityMethods - Done !

---
+++ H08_High_Quality_Code/S06_HighQualityMethods/E01_QualityMethods/Methods.cs
@@ -0,0 +1,230 @@
+﻿namespace E01_QualityMethods
+{
+    using System;
+
+    public class Methods
+    {
+        public static double CalculateTriangleArea(double sideA, double sideB, double sideC)
+        {
+            if (sideA <= 0 || sideB <= 0 || sideC <= 0)
+            {
+                //Console.Error.WriteLine("Sides should be positive.");
+                //return -1;
+                throw new ArgumentException("The sides should be positive " + 
+                    "numbers bigger than zero !");
+            }
+
+            double semiperimeter = (sideA + sideB + sideC) / 2;
+
+            double triangleArea = Math.Sqrt(semiperimeter * (semiperimeter - sideA) *
+                (semiperimeter - sideB) * (semiperimeter - sideC));
+
+            return triangleArea;
+        }
+
+        public static string DigitToEnglishName(int digit)
+        {
+            switch (digit)
+            {
+                case 0:
+                    {
+                        return "zero";
+                    }
+                case 1:
+                    {
+                        return "one";
+                    }
+                case 2:
+                    {
+                        return "two";
+                    }
+                case 3:
+                    {
+                        return "three";
+                    }
+                case 4:
+                    {
+                        return "four";
+                    }
+                case 5:
+                    {
+                        return "five";
+                    }
+                case 6:
+                    {
+                        return "six";
+                    }
+                case 7:
+                    {
+                        return "seven";
+                    }
+                case 8:
+                    {
+                        return "eight";
+                    }
+                case 9:
+                    {
+                        return "nine";
+                    }
+                default:
+                    {
+                        throw new ArgumentOutOfRangeException("Invalid number ! " + 
+                            "The number is out of range !");
+                    }
+            }
+        }
+
+        public static int FindBiggestElement(params int[] elements)
+        {
+            if (elements == null || elements.Length == 0)
+            {
+                //return -1;
+                throw new ArgumentNullException("The array cannot be null, or " + 
+                    "with length equal to zero !");
+            }
+
+            int biggestElement = int.MinValue;
+
+            for (int index = 1; index < elements.Length; index++)
+            {
+                if (elements[index] > biggestElement)
+                {
+                    biggestElement = elements[index];
+                }
+            }
+
+            return biggestElement;
+        }
+
+        public static void PrintNumberInFormat(object numberObj, string signForFormat)
+        {
+            //if (format == "f")
+            //{
+            //    Console.WriteLine ("{0:f2}", number);
+            //}
+            //if (format == "%")
+            //{
+            //    Console.WriteLine ("{0:p0}", number);
+            //}
+            //if (format == "r")
+            //{
+            //    Console.WriteLine ("{0,8}", number);
+            //}
+
+            decimal number = ObjectToDecimal(numberObj);
+
+            string format = GetFormatForPrint(signForFormat);
+
+            Console.WriteLine(format, number);
+        }
+
+        public static double CalculateDistance(double x1, double y1, double x2, double y2)
+        {
+            double distance = Math.Sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+
+            return distance;
+        }
+
+        // out bool isHorizontal, out bool isVertical
+        // isHorizontal = (y1 == y2);
+        // isVertical = (x1 == x2);
+
+        public static bool IsHorizontal(double y1, double y2)
+        {
+            bool isHorizontal = (y1 == y2);
+
+            return isHorizontal;
+        }
+
+        public static bool IsVertical(double x1, double x2)
+        {
+            bool isVertical = (x1 == x2);
+
+            return isVertical;
+        }
+
+        private static decimal ObjectToDecimal(object numberObject)
+        {
+            decimal newDecimalNumber;
+
+            try
+            {
+                newDecimalNumber = Convert.ToDecimal(numberObject);
+            }
+            catch (FormatException)
+            {
+                throw new FormatException("The input value is not a number !");
+            }
+
+            return newDecimalNumber;
+        }
+
+        private static string GetFormatForPrint(string format)
+        {
+            switch (format)
+            {
+                case "f":
+                    {
+                        return "{0:f2}";
+                    }
+                case "%":
+                    {
+                        return "{0:p0}";
+                    }
+                case "r":
+                    {
+                        return "{0,8}";
+                    }
+                default:
+                    {
+                        throw new ArgumentException("Invalid format ! " + 
+                            "Not supported format for print !");
+                    }
+            }
+        }
+               
+        
+        public static void Main()
+        {
+            Console.WriteLine("Triangle area : {0}", CalculateTriangleArea(3, 4, 5));
+
+            Console.WriteLine("Digit to english name : {0}", DigitToEnglishName(5));
+
+            Console.Write("Find biggest element (5, -1, 3, 2, 14, 2, 3) : ");
+            Console.WriteLine(FindBiggestElement(5, -1, 3, 2, 14, 2, 3));
+
+            Console.WriteLine();
+            Console.WriteLine("Print number in format:");
+            PrintNumberInFormat(1.3, "f");
+            PrintNumberInFormat(0.75, "%");
+            PrintNumberInFormat(2.30, "r");
+
+            Console.WriteLine();
+            double distance = CalculateDistance(3, -1, 3, 2.5);
+            Console.WriteLine("Distance = {0}", distance);
+
+            bool horizontal = IsHorizontal(-1, 2.5);
+            bool vertical = IsVertical(3, 3);
+            Console.WriteLine("Horizontal ? -> " + horizontal);
+            Console.WriteLine("Vertical ? -> " + vertical);
+
+            Student peter = new Student("Peter", "Ivanov");
+            peter.Location = "Sofia";
+            peter.DateOfBirth = "17.03.1992";
+            //peter.OtherInfo = "From Sofia, born at 17.03.1992";
+
+            Student stella = new Student("Stella", "Markova");
+            stella.Location = "Vidin";
+            stella.DateOfBirth = "03.11.1993";
+            stella.Profession = "gamer";
+            stella.OtherInfo = "high results";
+            //stella.OtherInfo = "From Vidin, gamer, high results, born at 03.11.1993";
+
+            Console.WriteLine();
+            Console.WriteLine("Peter date of birth: {0}", peter.DateOfBirth);
+            Console.WriteLine("Stella date of birth: {0}", stella.DateOfBirth);
+            Console.WriteLine("{0} is older than {1} -> {2}",
+                peter.FirstName, stella.FirstName, peter.IsOlderThan(stella));
+        }
+    }
+}
---
+++ H08_High_Quality_Code/S06_HighQualityMethods/E01_QualityMethods/Student.cs
@@ -0,0 +1,153 @@
+﻿namespace E01_QualityMethods
+{
+    using System;
+    using System.Globalization;
+
+    public class Student
+    {
+        private string firstName;
+        private string lastName;
+        private string dateOfBirth;
+        private string location;
+        private string profession;
+              
+        public Student(string firstNameValue, string lastNameValue)
+        {
+            this.FirstName = firstNameValue;
+            this.LastName = lastNameValue;
+        }
+
+        public string FirstName
+        {
+            get
+            {
+                return this.firstName;
+            }
+
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("\"First name\" cannot be " + 
+                        "null, empty, or consists only of white-space characters !");
+                }
+
+                this.firstName = value;
+            }
+        }
+
+        public string LastName
+        {
+            get
+            {
+                return this.lastName;
+            }
+
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("\"Last name\" cannot " + 
+                        "be null, empty, or consists only of white-space characters !");
+                }
+
+                this.lastName = value;
+            }
+        }
+
+        public string DateOfBirth
+        {
+            get
+            {
+                return this.dateOfBirth;
+            }
+
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("\"Date Of Birth\" cannot " + 
+                        "be null, empty, or consists only of white-space characters !");
+                }
+
+                DateTime dateOfBirthValue;
+
+                bool isDate = DateTime.TryParseExact(value, "dd.MM.yyyy", 
+                    CultureInfo.InvariantCulture, DateTimeStyles.None,
+                    out dateOfBirthValue);
+
+                if (isDate == false)
+                {
+                    throw new ArgumentException("Invalid date format !");
+                }
+
+                this.dateOfBirth = dateOfBirthValue.ToShortDateString();
+            }
+        }               
+
+        public string Location
+        {
+            get
+            {
+                return this.location;
+            }
+
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("\"Location\" cannot " + 
+                        "be null, empty, or consists only of white-space characters !");
+                }
+
+                this.location = value;
+            }
+        }
+
+        public string Profession
+        {
+            get
+            {
+                return this.profession;
+            }
+
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("\"Profession\" cannot " + 
+                        "be null, empty, or consists only of white-space characters !");
+                }
+
+                this.profession = value;
+            }
+        }
+
+        //private bool IsData (string dataValue)
+        //{
+        //    DateTime dateOfBirthValue;
+
+        //    bool isDate = DateTime.TryParseExact (dataValue, "dd.MM.yyyy", CultureInfo.InvariantCulture, DateTimeStyles.None,
+        //        out dateOfBirthValue);
+
+        //    return isDate;
+        //}
+
+        public string OtherInfo { get; set; }
+
+        public bool IsOlderThan(Student secondStudent)
+        {
+            //DateTime firstDate =
+            //    DateTime.Parse(this.OtherInfo.Substring(this.OtherInfo.Length - 10));
+            //DateTime secondDate =
+            //    DateTime.Parse(other.OtherInfo.Substring(other.OtherInfo.Length - 10));
+            //return firstDate > secondDate;
+
+            bool isOlder = false;
+
+            isOlder = (Convert.ToDateTime(this.DateOfBirth) < Convert.ToDateTime(secondStudent.DateOfBirth));
+
+            return isOlder;
+        }
+    }
+}
