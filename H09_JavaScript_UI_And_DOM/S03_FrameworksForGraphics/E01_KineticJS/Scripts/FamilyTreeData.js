961bce36649371bd7e9d76786db46b1725880a91
Boray <boray@abv.bg>
2015-07-25 21:55:54 +0300

    S02_ScallableVectorGraphics and S03_FrameworksForGraphics - Done !

---
+++ H09_JavaScript_UI_And_DOM/S03_FrameworksForGraphics/E01_KineticJS/Scripts/FamilyTreeData.js
@@ -0,0 +1,16 @@
+﻿
+var Data = (function () {
+    var familyMembers = [{
+        mother: 'Maria Petrova',
+        father: 'Georgi Petrov',
+        children: ['Teodora Petrova', 'Peter Petrov']
+    }, {
+        mother: 'Petra Stamatova',
+        father: 'Todor Stamatov',
+        children: ['Maria Petrova']
+    }];
+
+    return {
+        familyMembers: familyMembers
+    };
+}() )
