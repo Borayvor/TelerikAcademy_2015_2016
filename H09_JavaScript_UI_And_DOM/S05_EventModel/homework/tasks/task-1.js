7acd25b6d9b9312bc578b441ae672f95285b5c80
Boray <boray@abv.bg>
2015-08-01 20:53:57 +0300

    S04_DOM_Operations - Done ! S05_EventModel - Done !

---
+++ H09_JavaScript_UI_And_DOM/S05_EventModel/homework/tasks/task-1.js
@@ -0,0 +1,81 @@
+/* globals $ */
+
+/* 
+
+Create a function that takes an id or DOM element and:
+  
+
+*/
+
+function solve() {
+    return function ( selector ) {
+        var i,
+            len,
+            children,
+            currentChild,
+            content;
+
+        if ( arguments.length === 0 ) {
+            throw new Error();
+        }
+
+        if ( selector === undefined || selector === null ) {
+            throw new Error();
+        }
+
+        if ( typeof selector !== 'string' && !( selector instanceof HTMLElement ) ) {
+            throw new Error();
+        }
+
+        var element = document.getElementById( selector );
+
+        if ( element === null ) {
+            throw new Error();
+        }
+
+        children = element.childNodes;
+        len = children.length;
+
+        for ( i = 0; i < len; i += 1 ) {
+            currentChild = children[i];
+
+            if ( currentChild.className === 'button' ) {
+                currentChild.innerHTML = 'hide';
+
+                currentChild.addEventListener( 'click', function ( ev ) {
+                    var clickedButton = ev.target;
+                    var nextElement = clickedButton.nextElementSibling;
+
+                    while ( nextElement ) {
+                        if ( nextElement.className === 'content' ) {
+                            content = nextElement;
+
+                            while ( nextElement ) {
+                                if ( nextElement.className === 'button' ) {
+
+                                    if ( content.style.display === 'none' ) {
+                                        content.style.display = '';
+                                        clickedButton.innerHTML = 'hide';
+                                    } else {
+                                        content.style.display = 'none';
+                                        clickedButton.innerHTML = 'show';
+                                    }
+
+                                    break;
+                                }
+
+                                nextElement = nextElement.nextElementSibling;
+                            }
+
+                            break;
+                        } else {
+                            nextElement = nextElement.nextElementSibling;
+                        }
+                    }
+                } );
+            }
+        }
+    };
+};
+
+module.exports = solve;
