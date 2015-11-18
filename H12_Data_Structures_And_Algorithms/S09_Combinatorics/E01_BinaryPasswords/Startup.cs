7539041f463ef1db5ef2f6a6ff98c1c10485dd61
Borayvor <boray@abv.bg>
2015-11-18 12:17:14 +0200

    S09_Combinatorics - Done !

---
+++ H12_Data_Structures_And_Algorithms/S09_Combinatorics/E01_BinaryPasswords/Startup.cs
@@ -0,0 +1,29 @@
+﻿namespace E01_BinaryPasswords
+{
+    using System;
+
+    public class Startup
+    {
+        public static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            int baseOfNumeralSystem = 2;
+            ulong count = 0;
+
+            foreach (var symbol in input)
+            {
+                if (symbol == '0' || symbol == '1')
+                {
+                    continue;
+                }
+
+                count++;
+            }
+
+            ulong numberPossibleBinaryPasswords = (ulong)Math.Pow(baseOfNumeralSystem, count);
+
+            Console.WriteLine(numberPossibleBinaryPasswords);
+        }
+    }
+}
---
+++ H12_Data_Structures_And_Algorithms/S09_Combinatorics/E02_ColoredRabits/Startup.cs
@@ -0,0 +1,54 @@
+﻿namespace E02_ColoredRabits
+{
+    using System;
+
+    public class Startup
+    {
+        public static void Main(string[] args)
+        {
+            int rabitsCount = int.Parse(Console.ReadLine());
+
+            int[] replies = new int[rabitsCount];
+
+            for (int i = 0; i < rabitsCount; i++)
+            {
+                replies[i] = int.Parse(Console.ReadLine());
+            }
+
+            int answer = GetMinimumRabits(replies);
+
+            Console.WriteLine(answer);
+        }
+
+        private static int GetMinimumRabits(int[] replies)
+        {
+            int[] cache = new int[1000002];
+
+            for (int i = 0; i < replies.Length; i++)
+            {
+                cache[replies[i] + 1]++;
+            }
+
+            int minCount = 0;
+
+            for (int i = 0; i < cache.Length; i++)
+            {
+                if (cache[i] == 0)
+                {
+                    continue;
+                }
+
+                if (cache[i] <= i)
+                {
+                    minCount += i;
+                }
+                else
+                {
+                    minCount += (int)Math.Ceiling((double)cache[i] / i) * i;
+                }
+            }
+
+            return minCount;
+        }
+    }
+}
