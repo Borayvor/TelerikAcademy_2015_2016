b3e8c6345fa31fbc7487df42c6c62fb20707b581
Borayvor <boray@abv.bg>
2015-11-24 11:49:51 +0200

    S11_DynamicProgramming - Done !

---
+++ H12_Data_Structures_And_Algorithms/S11_DynamicProgramming/E01_KnapsackProblem/Item.cs
@@ -0,0 +1,19 @@
+﻿namespace E01_KnapsackProblem
+{
+    public struct Item
+    {
+        public Item(string name, int weight, int value)
+            : this()
+        {
+            this.Name = name;
+            this.Weight = weight;
+            this.Value = value;
+        }
+
+        public string Name { get; private set; }
+
+        public int Weight { get; private set; }
+
+        public int Value { get; private set; }
+    }
+}
